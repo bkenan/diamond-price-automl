4c0e0c4a8996f9bf07647fb1de811dde2744bbb0
Kenan Bekirli <53462948+bkenan@users.noreply.github.com>
2022-07-04 14:51:41 -0400

    main script

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sagemaker
+
+def automl():
+  
+  df = pd.read_csv('./data/diamonds.csv')
+  session = sagemaker.Session()
+  
+  prefix = 'sagemaker/autopilot/input'
+  
+  uri = session.upload_data(path = './data/diamonds.csv', key_prefix = prefix)
+
+  #print(uri): "s3://sagemaker-us-east-1-082380823452/sagemaker/autopilot/input/./data/diamonds.csv"
+  output = 's3://sagemaker-us-east-1-082380823452/sagemaker/autopilot/output'
+
+  
+if __name__ == "__main__":
+    automl()
